8b0f82c5a18daf55eeac204449543cb09660efc6
AX - Scripts <108193124+JustAuramo@users.noreply.github.com>
2023-10-20 13:46:17 +0300

    Add files via upload

---
+++ ShowTextUi.lua
@@ -0,0 +1,9 @@
+lib.showTextUI(message){
+    position = "left-center",
+    icon = icon or '',
+    style = {
+        borderRadius= 5,
+        backgroundColor = '#0985e3f8',
+        color = 'white'
+    }
+})
---
+++ libNotify.lua
@@ -0,0 +1,15 @@
+lib.notify({
+    id = 'ilmotus',
+    title = 'ILMOITUS',
+    description = 'Notification description',
+    position = 'top-right',
+    style = {
+        backgroundColor = '#141517',
+        color = '#C1C2C5',
+        ['.description'] = {
+          color = '#909296'
+        }
+    },
+    icon = 'fas fa-bell',
+    iconColor = '#C53030'
+})
